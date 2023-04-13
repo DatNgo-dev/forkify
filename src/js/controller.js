8f239cfbeca8b3fe7ea22e033e817d15b8d7724a
Dat Ngo <dat_ngo10@protonmail.com>
2023-04-13 11:34:35 +0800

    Refactored project for MVC

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,10 @@
+import * as model from './model.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime'; // polyfill async await
+import recipeView from './views/recipeView.js';
+import recipeView from './views/recipeView.js';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +18,25 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async () => {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1. Loading
+    await model.loadRecipe(id);
+
+    // 2. Rendering
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
